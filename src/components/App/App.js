135441bff46e4d445ce10d3ac23704332fa15c8b
Misha Chernov <80amulet63@gmail.com>
2020-02-17 23:20:14 +0200

    lesson-134:ADD: Implemented Routing on the pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route, 
+  Switch} from 'react-router-dom';
+
 import Footer from '../Footer';
 import Header from '../Header';
+
+import HomePage from '../pages/HomePage';
+import CartPage from '../pages/CartPage';
 
 
 require('./app.scss');
@@ -11,14 +19,16 @@ function App(props) {
   return(
     <>
       <Header />
-      <main className="container">
-        {props.books.map(book => (
-          <>
-            <h2>{book.title}</h2>
-            <p>{book.author}</p>
-          </>
-        ))}
-      </main>
+      <Router>
+        <Switch>
+          <Route path='/cart'>
+            <CartPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
       <Footer />
     </>
   )
